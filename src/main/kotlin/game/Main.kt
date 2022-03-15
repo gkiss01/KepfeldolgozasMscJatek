b381bf4b6d6b366cf07189fecfc0fd40c33f90a6
gkiss01 <gergokiss05@gmail.com>
2022-03-15 11:37:27 +0100

    Játék irányítása kézzel

--- src/main/kotlin/game/Main.kt
+++ src/main/kotlin/game/Main.kt
@@ -12,11 +12,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -50,23 +48,27 @@ fun App(angle: Double) {
         game.updateGame()
     }
 
+    LaunchedEffect(key1 = angle) {
+        game.updateAngle(angle)
+    }
+
     MaterialTheme {
         Box(modifier = Modifier
             .fillMaxSize()
             .clipToBounds()
-            .pointerMoveFilter(onMove = {
-                with(density) {
-                    game.updatePointerLocation(DpOffset(it.x.toDp(), it.y.toDp()))
-                }
-                false
-            })
+//            .pointerMoveFilter(onMove = {
+//                with(density) {
+//                    game.updatePointerLocation(DpOffset(it.x.toDp(), it.y.toDp()))
+//                }
+//                false
+//            })
             .onSizeChanged {
                 with(density) {
                     game.width = it.width.toDp()
                     game.height = it.height.toDp()
                 }
-            })
-        {
+            }
+        ) {
             Column {
                 GameMap(game.gameObjects, game.cols)
                 Column(
